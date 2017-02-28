e9d2656d550e8ca0bf1c012f27c5b5f4efe42bf1
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-02-28 16:21:23 +0000

    cookie.js

--- cookie.js
+++ cookie.js
@@ -8,12 +8,28 @@ var escapeHtml = require('escape-html');
 var http = require('http');
 var url = require('url');
  
+var form = `
+<form method="GET">
+  <input placeholder="Enter your name" name="name"> 
+  <input type="submit" value="Set Name">
+</form>
+`;
+
 function onRequest(req, res) {
+  console.log(req.url);
   // Parse the query string 
   var query = url.parse(req.url, true, true).query;
+  console.log(query);
  
-  if (query && query.name) {
+  if (query && query.name) { // The input 'name' field was filled
     // Set a new cookie with the name 
+    /* 
+      The Set-Cookie header is sent by the server in response to
+      an HTTP request, which is used to create a cookie on the
+      user's system. The Cookie header is included by the client
+      application with an HTTP request sent to a server, if there
+      is a cookie that has a matching domain and path.
+    */
     res.setHeader('Set-Cookie', cookie.serialize('name', String(query.name), {
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 7 // 1 week 
@@ -35,14 +51,13 @@ function onRequest(req, res) {
   res.setHeader('Content-Type', 'text/html; charset=UTF-8');
  
   if (name) {
-    res.write('<p>Welcome back, <b>' + escapeHtml(name) + '</b>!</p>');
+    res.write(`<p>Welcome back, <b> ${escapeHtml(name)} </b>!</p>`);
   } else {
     res.write('<p>Hello, new visitor!</p>');
   }
  
-  res.write('<form method="GET">');
-  res.write('<input placeholder="enter your name" name="name"> <input type="submit" value="Set Name">');
-  res.end('</form');
+  res.write(form);
+  res.end();
 }
  
 http.createServer(onRequest).listen(3000);
