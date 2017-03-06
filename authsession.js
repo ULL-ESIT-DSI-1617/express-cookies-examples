5237291be2c9e0e505033025d807bd4f5c1d4e1c
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-03-06 21:57:57 +0000

    authsession.js

--- authsession.js
+++ authsession.js
@@ -3,6 +3,20 @@ var express = require('express'),
     session = require('express-session');
 var util = require("util");
 
+var instructions = `
+Visit these urls in the browser:
+<ul>
+  <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
+  <li> <a href="http://localhost:3000/login?username=juan&password=juanspassword">localhost:3000/login?username=juan&password=juanspassword</a> </li>
+  <li> <a href="http://localhost:3000/login?username=amy&password=amyspassword">localhost:3000/login?username=amy&password=amyspassword</a> </li>
+  <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
+  <li> <a href="http://localhost:3000/logout">localhost:3000/logout</a> </li>
+  <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
+</ul>
+`;
+
+var layout = function(x) { return x+"<br />\n"+instructions; };
+
 app.use(session({
     secret: '2C44-4D44-WppQ38S',
     resave: true,
@@ -20,41 +34,39 @@ var auth = function(req, res, next) {
   if (req.session && req.session.user === "amy" && req.session.admin)
     return next();
   else
-    return res.sendStatus(401);
+    return res.sendStatus(401); // https://httpstatuses.com/401
 };
  
 // Login endpoint
 app.get('/login', function (req, res) {
+  console.log(req.query);
   if (!req.query.username || !req.query.password) {
+    console.log('login failed');
     res.send('login failed');    
   } else if(req.query.username === "amy" || req.query.password === "amyspassword") {
     req.session.user = "amy";
     req.session.admin = true;
-    res.send("login success!");
+    res.send(layout("login success!"));
+  } else {
+    console.log(`login ${util.inspect(req.query)} failed`);    
+    res.send(layout(`login ${util.inspect(req.query)} failed`));    
   }
 });
  
 app.get('/', function(req, res) {
-  res.send(`
-    Visit these urls in the browser:
-    <ul>
-      <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
-      <li> <a href="http://localhost:3000/login?username=amy&password=amyspassword">localhost:3000/login?username=amy&password=amyspassword</a> </li>
-      <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
-      <li> <a href="http://localhost:3000/logout">localhost:3000/logout</a> </li>
-      <li> <a href="http://localhost:3000/content">localhost:3000/content</a> </li>
-    </ul>
-  `);
+  res.send(instructions);
 });
 // Logout endpoint
 app.get('/logout', function (req, res) {
   req.session.destroy();
-  res.send("logout success!");
+  res.send(layout("logout success!"));
 });
  
 // Get content endpoint
-app.get('/content', auth, function (req, res) {
-    res.send("You can only see this after you've logged in.");
+app.get('/content', 
+    auth,  // next only if authenticated
+    function (req, res) {
+    res.send(layout("You are authenticated. You can only see this after you've logged in."));
 });
  
 app.listen(3000);
